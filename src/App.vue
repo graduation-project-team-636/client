7051328ba388bb1da5e2b2256735c98a136ec9de
watchcat2k <810549046@qq.com>
2020-01-13 21:01:50 +0800

    基本完成主页布局

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "app"
 };
 </script>
 
@@ -29,8 +15,14 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 0px;
+  margin: 0px;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  border: hidden;
+  position: absolute;
 }
 </style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <el-row id="head">
+      <el-col :span="24"><Headbar></Headbar></el-col>
+    </el-row>
+
+    <el-row id="display" :gutter="0" type="flex" justify="center">
+      <el-col :xs="0" :sm="6" :md="5" :lg="4" :xl="3"
+        ><Sidebar></Sidebar
+      ></el-col>
+      <el-col :xs="24" :sm="18" :md="15" :lg="14" :xl="11"
+        ><Carousel></Carousel
+      ></el-col>
+    </el-row>
+
+    <el-row id="display" :gutter="0" type="flex" justify="center">
+      <el-col :xs="24" :sm="24" :md="20" :lg="18" :xl="14"
+        ><Recommend></Recommend
+      ></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ 相当于 /src
+import Headbar from "@/components/home/Headbar.vue";
+import Sidebar from "@/components/home/Sidebar.vue";
+import Carousel from "@/components/home/Carousel.vue";
+import Recommend from "@/components/home/Recommend.vue";
+
+export default {
+  name: "home",
+  components: {
+    Headbar,
+    Sidebar,
+    Carousel,
+    Recommend
+  }
+};
+</script>
+
+<style>
+#display {
+  margin-top: 20px;
+}
+</style>
