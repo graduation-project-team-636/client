536cace60edee665e41eb124b5233cff16c5a622
watchcat2k <810549046@qq.com>
2020-01-16 23:16:44 +0800

    完成了admin布局

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="admin">
     <el-container style="height: 100%;">
-      <el-header><Headbar></Headbar> </el-header>
+      <el-header style="padding:0px;"><Headbar></Headbar></el-header>
       <el-container>
         <el-aside width="200px"> <Sidebar></Sidebar></el-aside>
         <el-container>
